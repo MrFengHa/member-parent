9573f2d1f76f622f48b98621bf58181d68170f12
MrFengHa <1109179567@qq.com>
2021-03-24 15:55:23 +0800

    Initial commit

---
+++ member-eureka/src/main/java/com/home/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 文件描述
+ *
+ * @Author 冯根源
+ * @create 2021/3/24 15:32
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
