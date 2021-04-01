4f55e855ccdb53675ae84b1cbfde3af88717aec0
MrFengHa <1109179567@qq.com>
2021-04-01 15:57:15 +0800

    进入我的众筹前端页面

--- member-authentication-consumer/src/main/java/com/home/config/CrowdWebMvcConfig.java
+++ member-authentication-consumer/src/main/java/com/home/config/CrowdWebMvcConfig.java
@@ -12,19 +12,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class CrowdWebMvcConfig implements WebMvcConfigurer {
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         //浏览器访问的地址
         String regUrlPath = "/auth/member/to/reg/page";
         String loginUrlPath = "/auth/member/to/login/page";
         String memberCenterUrlPath = "/auth/member/to/center/page";
+        String myCrowdUrlPath = "/member/my/crowd";
 
         //访问视图的名称  将来拼接前后缀
         String regViewName="member-reg";
         String loginViewName="member-login";
         String memberCenterViewName="member-center";
+        String myCrowdViewName="member-crowd";
         //添加一个view-controller
         registry.addViewController(regUrlPath).setViewName(regViewName);
         registry.addViewController(loginUrlPath).setViewName(loginViewName);
         registry.addViewController(memberCenterUrlPath).setViewName(memberCenterViewName);
+        registry.addViewController(myCrowdUrlPath).setViewName(myCrowdViewName);
     }
 }
